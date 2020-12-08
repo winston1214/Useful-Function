4bc7151296512fc0d401e20517ef0862a5325efd
winston1214 <winston1214@naver.com>
2020-12-08 23:46:44 +0900

    timedelta to datetime(h,m,s)

---
+++ timedelta_to_datetime.py
@@ -0,0 +1,7 @@
+import datetime
+def convert_timedelta(duration):
+    days, seconds = duration.days, duration.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 60)
+    return hours, minutes, seconds

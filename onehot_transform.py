2519a976226f73a7933160f40665a57686a56c65
winston1214 <winston1214@naver.com>
2020-12-09 00:08:10 +0900

    OnehotEncoding Transformer

---
+++ onehot_transform.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder # onehotencoding dataframe 변환 함수
+def ohe_trans(data,col):
+    ohe=OneHotEncoder()
+    x= ohe.fit_transform(data[col].values.reshape(-1,1)).toarray()
+    tp = []
+    for i in range(data[col].unique().size): # onehot 컬럼 생성
+        tp.append(col[0]+str(i)) 
+    ohe_df = pd.DataFrame(x,columns = tp)
+    return ohe_df

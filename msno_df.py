8541a91f1bc7c3043955352ae0ae7cdcae567cd5
winston1214 <winston1214@naver.com>
2020-12-10 23:47:38 +0900

    missing value count df

---
+++ msno_df.py
@@ -0,0 +1,13 @@
+import pandas as pd
+def missing_per(df):
+    ms=pd.DataFrame(columns=['col','missing'])
+    idx = 0
+    for i in range(df.shape[1]):
+        if df.isnull().sum()[i]>0:
+            ms.loc[idx,'col'] = df.isnull().sum().index[i]
+            ms.loc[idx,'missing'] = df.isnull().sum()[i]/df.shape[0] * 100
+            idx+=1
+        else:
+            continue
+    ms=ms.sort_values(by='missing',ascending=False)
+    return ms

f3e920934bde1210dadd0374a9adb1ae35b2efb9
winston1214 <47775179+winston1214@users.noreply.github.com>
2021-02-22 20:51:52 +0900

    Update msno_df.py

--- msno_df.py
+++ msno_df.py
@@ -2,8 +2,7 @@
 import pandas as pd
 def missing_per(df):
     ms=pd.DataFrame(columns=['col','missing'])
-    idx = 0
-    for i in range(df.shape[1]):
+    for idx,i in enumerate(df.shape[1]):
         if df.isnull().sum()[i]>0:
             ms.loc[idx,'col'] = df.isnull().sum().index[i]
             ms.loc[idx,'missing'] = df.isnull().sum()[i]/df.shape[0] * 100

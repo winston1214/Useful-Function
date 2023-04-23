6f31be785cab1133b027f6bea461e8f0979ffeeb
YoungMin KIM <47775179+winston1214@users.noreply.github.com>
2023-04-23 17:09:08 +0900

    Update random_seed.py

--- random_seed.py
+++ random_seed.py
@@ -1,11 +1,12 @@
 import random
 import torch.backends.cudnn as cudnn
+import numpy as np
 
 def random_seed(seed_num):
-  torch.manual_seed(seed_num)
-  torch.cuda.manual_seed(seed_num)
-  torch.cuda.manual_seed_all(seed_num)
-  np.random.seed(seed_num)
-  cudnn.benchmark = False
-  cudnn.deterministic = True
-  random.seed(seed_num)
+    torch.manual_seed(seed_num)
+    torch.cuda.manual_seed(seed_num)
+    torch.cuda.manual_seed_all(seed_num)
+    np.random.seed(seed_num)
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(seed_num)
